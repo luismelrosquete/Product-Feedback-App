29ecf9ed0362c1b4de87719afb1ab46ab74b59f7
Luismel Rosquete <luismel.rosquete@gmail.com>
2022-02-12 16:28:52 -0500

    Added the About page and link to the homepage for users to learn more about this project.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,13 +1,16 @@
 import Card from "../components/shared/Card";
+import {Link} from "react-router-dom";
 
 function AboutPage() {
     return (
-        <div className="about">
-            <h1>About This Project</h1>
-            <p>This is a React app to leave feedback for a product or service.</p>
-            <p>Version: 1.0.0</p>
-            <p><a href="/">Back To Home</a></p>
-        </div>
+        <Card>
+            <div className="about">
+                <h1>About This Project</h1>
+                <p>This is a React app to leave feedback for a product or service.</p>
+                <p><strong>Version:</strong> 1.0.0</p>
+                <p><Link to="/">Back To Home</Link></p>
+            </div>
+        </Card>
     )
 }
 
